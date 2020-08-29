754ca2c51fbd58f9915e25494911c7f9ba55c262
FOZEU JEAN MARIE <fozeu.jm@gmail.com>
2020-08-29 22:41:08 +0200

    Test refactor ok!

--- test/com/kwd/ISBN/StockManagementTests.java
+++ test/com/kwd/ISBN/StockManagementTests.java
@@ -2,61 +2,49 @@ package com.kwd.ISBN;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.mockito.Mockito.*;
 
 public class StockManagementTests {
-	DataService testWebservice = mock(DataService.class);
-	DataService testDBservice = mock(DataService.class);
+	DataService testWebservice;
+	DataService testDBservice;
+	StockManager manager;
 
-	@Test 
+	@Before
+	public void setup() {
+		testWebservice = mock(DataService.class);
+		testDBservice = mock(DataService.class);
+		manager = new StockManager();
+		manager.setWebService(testWebservice);
+		manager.setDBservice(testDBservice);
+	}
+
+	@Test
 	public void testcanGetACorrectLocatorCode() {
 		String isbn = "0140177396";
-		when(testDBservice.lookUp(anyString())).thenReturn(new Book("of Mice And Men", "J. Steinbeck",isbn));
-		when(testWebservice.lookUp(anyString())).thenReturn(new Book("of Mice And Men", "J. Steinbeck",isbn));
-		StockManager manager = new StockManager();
-		manager.setDBservice(testDBservice);
-		manager.setWebService(testWebservice);
+		when(testDBservice.lookUp(anyString())).thenReturn(new Book("of Mice And Men", "J. Steinbeck", isbn));
 		String locatorCode = manager.getLocatorCode(isbn);
 		assertEquals("7396J4", locatorCode);
 	}
-	
+
 	@Test
 	public void databaseIsUsedIFDataIsPresent() {
 		String isbn = "0140177396";
-		DataService DBservice = mock(DataService.class);
-		DataService Webservice = mock(DataService.class);
-		
-		when(DBservice.lookUp(isbn)).thenReturn(new Book("abc","abc",isbn));
-		
-		StockManager manager = new StockManager();
-		manager.setDBservice(DBservice);
-		manager.setWebService(Webservice);
-		
-		String locatorCode = manager.getLocatorCode(isbn);
-		
-		verify(DBservice,times(1)).lookUp(isbn);
-		verify(Webservice,never()).lookUp(anyString());
+		when(testDBservice.lookUp(isbn)).thenReturn(new Book("abc", "abc", isbn));
+		manager.getLocatorCode(isbn);
+		verify(testDBservice, times(1)).lookUp(isbn);
+		verify(testWebservice, never()).lookUp(anyString());
 	}
-	
+
 	@Test
 	public void webServiceIsUsedIfDataIsNotPresentInDatabase() {
 		String isbn = "0140177396";
-		DataService DBservice = mock(DataService.class);
-		DataService Webservice = mock(DataService.class);
-		
-		when(DBservice.lookUp(isbn)).thenReturn(null);
-		
-		when(Webservice.lookUp(isbn)).thenReturn(new Book("abc","abc",isbn));
-		
-		StockManager manager = new StockManager();
-		manager.setDBservice(DBservice);
-		manager.setWebService(Webservice);
-		
-		String locatorCode = manager.getLocatorCode(isbn);
-		
-		verify(DBservice,times(1)).lookUp(isbn);
-		verify(Webservice).lookUp("0140177396");
+		when(testDBservice.lookUp(isbn)).thenReturn(null);
+		when(testWebservice.lookUp(isbn)).thenReturn(new Book("abc", "abc", isbn));
+		manager.getLocatorCode(isbn);
+		verify(testDBservice, times(1)).lookUp(isbn);
+		verify(testWebservice).lookUp("0140177396");
 	}
 }
