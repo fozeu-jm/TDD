4932bc4b9a85fee54ed998db088c98cb8cfe29ee
FOZEU JEAN MARIE <fozeu.jm@gmail.com>
2020-08-29 13:51:18 +0200

    Refactoring ok!

--- src/com/kwd/ISBN/ValidateISBN.java
+++ src/com/kwd/ISBN/ValidateISBN.java
@@ -2,35 +2,46 @@ package com.kwd.ISBN;
 
 public class ValidateISBN {
 
+	private static final int LONG_ISBN_LENGTH = 13;
+	private static final int SHORT_ISBN_LENGTH = 10;
+
 	public boolean checkISBN(String isbn) {
 		isbn = isbn.toUpperCase();
 
-		if (!(isbn.length() == 10 || isbn.length() == 13))
-			throw new NumberFormatException("Isbn number must be 10 or 13 digits");
+		if (isbn.length() == LONG_ISBN_LENGTH) {
+			return isValidLongISBN(isbn);
+		} else if (isbn.length() == SHORT_ISBN_LENGTH) {
+			return isValidShortISBN(isbn);
+		}
+		throw new NumberFormatException("Isbn number must be 10 or 13 digits long");
+	}
 
-		if (isbn.length() == 13) {
-			int total = 0, multiplier = 0;
-			for (int i = 0; i < isbn.length(); i++) {
-				multiplier = (i % 2 == 0) ? 1 : 3;
-				if (!Character.isDigit(isbn.charAt(i))) {
-					throw new NumberFormatException("Isbn number can only contain digits");
-				}
-				total += (Character.getNumericValue(isbn.charAt(i))) * multiplier;
+	private boolean isValidShortISBN(String isbn) {
+		int total = 0;
+		for (int i = 0; i < SHORT_ISBN_LENGTH; i++) {
+			if (i == 9 && isbn.charAt(i) == 'X') {
+				total += 10;
+			} else {
+				checkIfDigit(isbn.charAt(i));
+				total += (Character.getNumericValue(isbn.charAt(i))) * (SHORT_ISBN_LENGTH - i);
 			}
-			return ((total % 10) == 0);
-		} else {
-			int total = 0;
-			for (int i = 0; i < isbn.length(); i++) {
-				if (i == 9 && isbn.charAt(i) == 'X') {
-					total += 10;
-				} else {
-					if (!Character.isDigit(isbn.charAt(i))) {
-						throw new NumberFormatException("Isbn number can only contain digits");
-					}
-					total += (Character.getNumericValue(isbn.charAt(i))) * (10 - i);
-				}
-			}
-			return ((total % 11) == 0);
+		}
+		return ((total % 11) == 0);
+	}
+
+	private boolean isValidLongISBN(String isbn) {
+		int total = 0, multiplier = 0;
+		for (int i = 0; i < LONG_ISBN_LENGTH; i++) {
+			checkIfDigit(isbn.charAt(i));
+			multiplier = (i % 2 == 0) ? 1 : 3;
+			total += (Character.getNumericValue(isbn.charAt(i))) * multiplier;
+		}
+		return ((total % 10) == 0);
+	}
+
+	private void checkIfDigit(char c) {
+		if (!Character.isDigit(c)) {
+			throw new NumberFormatException("Isbn number can only contain digits");
 		}
 	}
 
